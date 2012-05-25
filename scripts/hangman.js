b70e235e74f9b152079deeb3f9c67d3616d0c7e8
Varun Baker <varun@mini-V.(none)>
2012-05-24 23:32:58 -0400

    prt 2 adding canvas and polyfills

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -65,8 +65,119 @@ function getLetter() {
 
 // Check whether selected letter is in the word to be guessed
 function checkLetter(letter) {
-	
+   var placeholders = word.innerHTML,
+       wrongGuess = true;
+   // split the placeholders into an array
+   placeholders = placeholders.split('');
+   // loop through the array
+   for (var i = 0; i < wordLength; i++) {
+      // if the selected letter matches one in the word to guess,
+      // replace the underscore and increase the number of correct guesses
+      if (wordToGuess.charAt(i) == letter.toLowerCase()) {
+         placeholders[i] = letter;
+         wrongGuess = false;
+         correctGuesses++;
+         // redraw the canvas only if all letters have been guessed
+         if (correctGuesses == wordLength) {
+            drawCanvas();
+         }
+      }
+   }
+   // if the guess was incorrect, increment the number of bad
+   // guesses and redraw the canvas
+   if (wrongGuess) {
+      badGuesses++;
+      drawCanvas();
+   }
+   // convert the array to a string and display it again
+   word.innerHTML = placeholders.join('');
 }
+
+// Draw the canvas
+function drawCanvas() {
+    var c = canvas.getContext('2d');
+    // reset the canvas and set basic styles
+    canvas.width = canvas.width;
+    c.lineWidth = 10;
+    c.strokeStyle = 'green';
+    c.font = 'bold 24px Optimer, Arial, Helvetica, sans-serif';
+    c.fillStyle = 'red';
+    // draw the ground
+    drawLine(c, [20,190], [180,190]);
+    // start building the gallows if there's been a bad guess
+    if (badGuesses > 0) {
+        // create the upright
+        c.strokeStyle = '#A52A2A';
+        drawLine(c, [30,185], [30,10]);
+        if (badGuesses > 1) {
+            // create the arm of the gallows
+            c.lineTo(150,10);
+            c.stroke();
+        }
+        if (badGuesses > 2) {
+            c.strokeStyle = 'black';
+            c.lineWidth = 3;
+            // draw rope
+            drawLine(c, [145,15], [145,30]);
+            // draw head
+            c.beginPath();
+            c.moveTo(160, 45);
+            c.arc(145, 45, 15, 0, (Math.PI/180)*360);
+            c.stroke(); 
+        }
+        if (badGuesses > 3) {
+            // draw body
+            drawLine(c, [145,60], [145,130]);
+        }
+        if (badGuesses > 4) {
+            // draw left arm
+            drawLine(c, [145,80], [110,90]);
+        }
+        if (badGuesses > 5) {
+            // draw right arm
+            drawLine(c, [145,80], [180,90]);
+        }
+        if (badGuesses > 6) {
+            // draw left leg
+            drawLine(c, [145,130], [130,170]);
+        }
+        if (badGuesses > 7) {
+            // draw right leg and end game
+            drawLine(c, [145,130], [160,170]);
+            c.fillText('Game over', 45, 110);
+            // remove the alphabet pad
+            letters.innerHTML = '';
+            // display the correct answer
+            // need to use setTimeout to prevent race condition
+            setTimeout(showResult, 200);
+            // increase score of lost games
+            // display the score after two seconds
+            // code to be added later
+        }
+    }
+    // if the word has been guessed correctly, display message,
+    // update score of games won, and then show score after 2 seconds
+    if (correctGuesses == wordLength) {
+        letters.innerHTML = '';
+        c.fillText('You won!', 45,110);
+        // increase score of won games
+        // display score
+        // code to be added later
+    }
+}
+
+// When the game is over, display missing letters in red
+function showResult() {
+    var placeholders = word.innerHTML;
+    placeholders = placeholders.split('');
+    for (i = 0; i < wordLength; i++) {
+        if (placeholders[i] == '_') {
+        placeholders[i] = '<span style="color:red">' + wordToGuess.charAt(i).toUpperCase() + '</span>';
+        }
+    }
+    word.innerHTML = placeholders.join('');
+}
+
 
 function resetScore() {
 	alert('Score has been reset');
